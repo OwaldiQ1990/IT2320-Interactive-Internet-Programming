750670c15cb70675472a1067e45fb31dd78ba6c9
OwaldiQ1990 <43704344+OwaldiQ1990@users.noreply.github.com>
2018-12-09 19:09:18 -0500

    Add files via upload

--- week10_11/Lab11/home.js
+++ week10_11/Lab11/home.js
@@ -1,86 +1,50 @@
 
-$(function(){
 
-//jQuery code goes here
-    $("#btn1").on("click", function(){
-        var url = "https://api.flickr.com/services/feeds/photos_public.gne?&format=json&jsoncallback=?&tags="
-                    +$("#photosearch").val();   
-       $.ajax({
+
+
+$(function(){
+    $("#btn3").on("click", function(){
+       
+      const key = "175dbeaded0d3090a961e0b364c47586"; //key ///
+      var animal= $("#animalsearch").val();
+      var breed = $("#breedsearch").val();
+      var size= $("#sizesearch").val();
+      var sex= $("#sexsearch").val();
+      var location= $("#locationsearch").val();
+ 
+        var url = "https://api.petfinder.com/pet.find?&format=json&key="+key+"&callback=?&animal="+animal+"&breed="+breed+"&size+"+size+"&sex="+sex+"&location="+location;
+       
+$.ajax({
            url: url,           
            type: "GET", 
            dataType: "jsonp",   //needed for CORS       
            contentType: "application/json; charset=utf-8",
            crossDomain: true,                   
            success: function(data){
-              console.log(data);  
+              console.log(data);
+              $("ul").remove();  
+              
+var ul = $("<ul>Animal:</ul>");
+         $("#results2").after(ul);
 
-              $.each(data.items, function(i, item){                
-                var img = $("<img></img>");
-                img.attr("src",item.media.m);
-                $("#flikrresults").append(img);
-              });              
+              $.each(data.petfinder.pets.pet,function(i,item)
+              {
+              var li = $("<p></p>").text(item.animal.$t);
+                $("ul").append(li);
+                var li = $("<p></p>").text(item.id.$t); 
+                  $("ul").append(li);
+                  var li = $("<p></p>").text(item.description.$t);
+               $("ul").append(li);
+               var li = $("<p></p>").text(item.name.$t);
+              $("ul").append(li);
+
+          
+              }
+             );              
            }
          });
     });
-
+  });
 
     
-    $("#btn2").on("click", function(){
-        //Go to https://www.petfinder.com/developers/api-key to get an api key
-        //PetFinder API Key 551b527add03634ac3a95a7a967367e6
-        //PetFinder API Secret 6a5a2731d31fb44b2d2303c7686c5278
-        
-        const key = "175dbeaded0d3090a961e0b364c47586";           
-        var url = "http://api.petfinder.com/breed.list?format=json&key="+key+"&callback=?&animal="+$("#breedsearch").val()+"&format=json";
-       $.ajax({
-           url: url,           
-           type: "GET", 
-           dataType: "jsonp",   //needed for CORS       
-           contentType: "application/json; charset=utf-8",
-           crossDomain: true,                   
-           success: function(data){              
-              console.log(data);
-
-              $("ul").remove();
-              var ul = $("<ul>Breeds</ul>");
-              $("#breedresults").after(ul);
-              
-              $.each(data.petfinder.breeds.breed,function(i,breed){                  
-                    var li = $("<li></li>").text(breed.$t);
-                    $("ul").append(li);
-              });                      
-            }                    
-         });//ajax
-    });//btn click   
-
-    $("#btn3").on("click", function(){
-
-      // http://api.petfinder.com/pet.find?callback=?&format=json&key=551b527add03634ac3a95a7a967367e6& 
-
-      //location=Ohio&size=S&sex=F&breed=Australian%20Shepherd&animal=dog&_=1541004833654
-
-
-        const key = "175dbeaded0d3090a961e0b364c47586";           
-        //var url = "http://api.petfinder.com/pet.find?callback=?&format=json&key="+key+"&location="+$("#breedsearch").val()+"Ohio&size=S&sex=F&breed=Australian%20Shepherd&animal=dog&_=1541004833654.val()+"&format=json";
-       var url = "http://api.petfinder.com/breed.list?format=json&key="+key+"&callback=?&animal="+$("#breedsearch2").val()+"&format=json";
-       $.ajax({
-           url: url,           
-           type: "GET", 
-           dataType: "jsonp",   //needed for CORS       
-           contentType: "application/json; charset=utf-8",
-           crossDomain: true,                   
-           success: function(data){              
-              console.log(data);
-              $("ul").remove();
-              var ul = $("<ul>Breeds</ul>");
-              $("#breedresults2").after(ul);
-              
-              $.each(data.petfinder.breeds.breed,function(i,breed){                  
-                    var li = $("<li></li>").text(breed.$t);
-                    $("ul").append(li);
-              });                      
-            }                    
-         });//ajax
-    });//btn click
-    
-})
+   
